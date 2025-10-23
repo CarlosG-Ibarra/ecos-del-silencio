7b175d1a13d3947d4d7bbbd3dfec7b0e37085937
Joel Gomez <150473829+Tetansaya@users.noreply.github.com>
2025-10-23 09:45:22 -0600

    Creacion de la pestaña de minijuegos con Bloques preasignados par cada minijuego

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MinijuegosComponent } from './minijuegos/minijuegos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'minijuegos', component: MinijuegosComponent },
+  // Aquí podrás agregar más rutas después:
+  // { path: 'contacto', component: ContactoComponent },
+  // { path: 'historia', component: HistoriaComponent },
+];
---
+++ src/app/minijuegos/minijuegos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-minijuegos',
+  standalone: true,
+  templateUrl: './minijuegos.html',
+  styleUrls: ['./minijuegos.css']
+})
+export class MinijuegosComponent {}
