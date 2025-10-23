ab79774151167e0c38254be1fd48b28b0e7687e6
CarlosG-Ibarra <carlosibarraa@live.com>
2025-10-23 09:51:45 -0600

    inicio done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './inicio/inicio';
+import { Caracteristicas } from './caracteristicas/caracteristicas';
+import { Historia } from './historia/historia';
+import { Contacto } from './contacto/contacto';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Inicio }, 
+  { path: 'caracteristicas', component: Caracteristicas },
+  { path: 'historia', component: Historia },
+  { path: 'contacto', component: Contacto },
+];
