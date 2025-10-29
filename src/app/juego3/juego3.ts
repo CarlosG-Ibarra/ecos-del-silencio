112dd7545eaae05e6e92ef568f2d2f06999e813c
Joel Gomez <150473829+Tetansaya@users.noreply.github.com>
2025-10-29 07:52:16 -0600

    Creacion de Componentes minijuegos

---
+++ src/app/juego3/juego3.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-juego3',
+  standalone: true,
+  templateUrl: './juego3.html',
+  styleUrls: ['./juego3.css']
+})
+export class Juego3Component {
+  mensaje = 'Aquí irá tu minijuego 3 🕹️';
+}
--- src/app/minijuegos/minijuegos.ts
+++ src/app/minijuegos/minijuegos.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Juego1Component } from '../juego1/juego1';
+import { Juego2Component } from '../juego2/juego2';
+import { Juego3Component } from '../juego3/juego3';
 
 @Component({
   selector: 'app-minijuegos',
   standalone: true,
+  imports: [CommonModule,  Juego1Component, Juego2Component, Juego3Component],
   templateUrl: './minijuegos.html',
   styleUrls: ['./minijuegos.css']
 })
-export class MinijuegosComponent {}
+export class MinijuegosComponent {
+  juegos = [
+    { id: 1, nombre: 'Juego 1', descripcion: 'Haz click para jugar' },
+    { id: 2, nombre: 'Juego 2', descripcion: 'Haz click para jugar' },
+    { id: 3, nombre: 'Juego 3', descripcion: 'Haz click para jugar' }
+  ];
+
+  seleccionado: number | null = null;
+
+  seleccionarJuego(id: number) {
+    this.seleccionado = this.seleccionado === id ? null : id;
+  }
+}
