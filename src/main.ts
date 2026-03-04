d82d40b058c78db0d365112c36362be17b4f3ff0
CarlosG-Ibarra <carlosibarraa@live.com>
2026-03-03 20:40:16 -0600

    Converted to pwa and changed icons

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { appConfig } from './app/app.config';
-import { environment } from './environments/environment';
 
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 
+// ✅ Explicitly import with .ts extension removed but correct relative path
+import { environment } from './environments/environment';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 bootstrapApplication(App, {
   ...appConfig,
   providers: [
     ...(appConfig.providers || []),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth())
+    provideAuth(() => getAuth()), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 });
-
